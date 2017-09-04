443fec58dab3365e0e179942b3e6e224fc9fde91
Aaron <aircitybruno@gmail.com>
2017-09-04 11:11:29 +0800

    Add color scheme

--- ini.go
+++ ini.go
@@ -5,9 +5,19 @@ import "image/color"
 var Config = Banny{
 	Rows: 5,
 	Foreground: []color.NRGBA{
-		rgb(45, 79, 255),
-		rgb(141, 69, 170),
-		rgb(49, 203, 115),
+		rgb(0, 0, 255),		// blue
+		rgb(160,32,240),	// purple
+		rgb(154, 205,50),	// yellowgreen
+		rgb(238, 130,238),	// violet
+		rgb(255,99,71),		// tomato
+		rgb(0,128,128),		// teal
+		rgb(0,255,127),		// springgreen
+		rgb(160,90,205),	// slateblue
+		rgb(112, 128,144),	// slategray
+		rgb(160,82,45),		// sienna
+		rgb(128,128,0),		// olive
+		rgb(210,105,30),	// chocolate
+		rgb(220,20,60),		// crimson
 	},
 	Background: rgb(240, 240, 240),
 }
